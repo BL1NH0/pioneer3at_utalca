cade0b3001d01331479a0b92470acfa242e418c2
Pablo Vallejos <166871308+BL1NH0@users.noreply.github.com>
2025-11-17 17:25:19 -0300

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "🔨 Construyendo imagen Docker..."
-docker build -t pioneer3at_utalca:latest .
-
-echo "✅ Imagen construida exitosamente!"
-echo ""
-echo "Para iniciar: ./start.sh"
